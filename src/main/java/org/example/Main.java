881f6d58637dc39f45c38f9e3ee8fd075e5b191a
Luigi <bar24056@uvg.edu.gt>
2025-03-25 16:33:34 -0600

    csv medio implementado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,12 @@
 package org.example;
 
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -15,9 +21,29 @@ public class Main {
             input = sc.nextLine();
 
 
-
         }
 
+    }
+
+    public static void csvfile() {
+
+        try (CSVReader reader = new CSVReader(new FileReader("SKUcsv.csv"))) {
+            List<String[]> rows = reader.readAll();
+            boolean isHeader = true;
+
+            for (String[] row : rows) {
+                if (isHeader) { // Saltar la primera fila si es encabezado
+                    isHeader = false;
+                    continue;
+                }
+
+            }
+        } catch (IOException | CsvException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+        //Estructura del metodo y manejo de excepciones extraida de: https://infogonzalez.com/2024/11/lectura-y-escritura-de-ficheros-csv-con-java.html
 
     }
+
 }
