3f9ce861b7824a03b8d3608e53d5a995b8334171
Luigi <bar24056@uvg.edu.gt>
2025-03-25 16:07:52 -0600

    ola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,22 @@
 package org.example;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        boolean seguir = true;
+
+        while(seguir){
+            System.out.println("Hola");
+            input = sc.nextLine();
+
+
+
+        }
 
 
     }
